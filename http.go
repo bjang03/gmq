0136ad4a1570ca6bd742a3720bebb2cae1c0122f
张斌 <259278618@qq.com>
2026-02-04 15:44:24 +0800

    .gitignore

---
+++ http.go
@@ -0,0 +1,13 @@
+package gmq
+
+import "net/http"
+
+func init() {
+	server := http.NewServeMux()
+	server.HandleFunc("/gmq", func(writer http.ResponseWriter, request *http.Request) {
+
+	})
+	go func() {
+		_ = http.ListenAndServe("", server)
+	}()
+}

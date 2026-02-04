77c8b4ecb17c5eda1a08d7717a2b49abebec8b26
张斌 <259278618@qq.com>
2026-02-04 18:54:01 +0800

    README.md

--- web/server.go
+++ web/server.go
@@ -8,6 +8,8 @@ func init() {
 	// 注册路由
 	HttpServer.Post("/publish", controller.Publish)
 	HttpServer.Get("/subscribe", controller.Subscribe)
+	HttpServer.Get("/metrics", controller.GetMetrics)
+	HttpServer.Get("/metrics/all", controller.GetAllMetrics)
 
 	go func() {
 		_ = HttpServer.Run(":1688")

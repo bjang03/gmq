07716d0e891ed9aecab0a8020627b8f2ea3c3939
张斌 <259278618@qq.com>
2026-02-06 17:38:36 +0800

    README.md

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
-	_ "github.com/bjang03/gmq/components"
+	"github.com/bjang03/gmq/components"
+	"github.com/bjang03/gmq/core"
 	_ "github.com/bjang03/gmq/web"
 )
 
 func main() {
-
+	core.GmqRegister("nats", &components.NatsConn{
+		URL:            "nats://localhost:4222",
+		Timeout:        10,
+		ReconnectWait:  5,
+		MaxReconnects:  -1,
+		MessageTimeout: 30,
+	})
 }

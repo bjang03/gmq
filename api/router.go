16a8f3c80184a87cf1c12c803527e355ac034c22
张斌 <259278618@qq.com>
2026-02-26 08:57:23 +0800

    README.md

--- api/router.go
+++ api/router.go
@@ -1,11 +1,47 @@
 package api
 
 import (
+	"context"
 	"log"
 	stdhttp "net/http"
 
+	"github.com/bjang03/gmq/core"
 	"github.com/bjang03/gmq/utils"
 )
+
+var serverManager *utils.ServerManager
+
+func InitGmq() {
+
+	// 设置路由 - 使用封装的ServeMux
+	mux := utils.NewServeMux()
+	SetupRouter(mux)
+
+	// 打印注册的路由
+	PrintRoutes(mux)
+
+	// 创建服务器管理器
+	serverManager = utils.NewServerManager(func(ctx context.Context) error {
+		if err := core.Shutdown(ctx); err != nil {
+			return err
+		}
+		StopMetricsBroadcast()
+		return nil
+	})
+
+	// 添加服务器
+	serverManager.AddServer(utils.NewServer(":1688", mux))
+
+	// 启动所有服务器（不阻塞）
+	serverManager.StartAll()
+}
+
+// StartGmqWithGracefulShutdown 启动服务器并等待优雅关闭（阻塞）
+func StartGmqWithGracefulShutdown(shutdownTimeout int) {
+	if serverManager != nil {
+		serverManager.WaitForShutdown(10)
+	}
+}
 
 // SetupRouter 设置路由
 func SetupRouter(mux *utils.ServeMux) {
--- main.go
+++ main.go
@@ -1,15 +1,13 @@
 package main
 
 import (
-	"context"
-
 	"github.com/bjang03/gmq/api"
 	"github.com/bjang03/gmq/core"
 	"github.com/bjang03/gmq/mq"
-	"github.com/bjang03/gmq/utils"
 )
 
 func main() {
+	api.InitGmq()
 	core.GmqRegister("redis", &mq.RedisConn{
 		Url: "localhost:6379",
 	})
@@ -24,25 +22,6 @@ func main() {
 		VHost:    "",
 	})
 
-	// 设置路由 - 使用封装的ServeMux
-	mux := utils.NewServeMux()
-	api.SetupRouter(mux)
-
-	// 打印注册的路由
-	api.PrintRoutes(mux)
-
-	// 创建服务器管理器
-	manager := utils.NewServerManager(func(ctx context.Context) error {
-		if err := core.Shutdown(ctx); err != nil {
-			return err
-		}
-		api.StopMetricsBroadcast()
-		return nil
-	})
-
-	// 添加服务器
-	manager.AddServer(utils.NewServer(":1688", mux))
-
-	// 启动所有服务器并支持优雅关闭
-	manager.StartWithGracefulShutdown(10)
+	// 阻塞等待关闭信号，触发优雅关闭
+	api.StartGmqWithGracefulShutdown(10)
 }
